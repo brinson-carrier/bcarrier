b2950159f746512b58536398e40bfa73e5a49f45
Brinson Carrier <brinsoncarrier@gamil.com>
2024-12-09 22:01:42 -0500

    static and deploy

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: "export",
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",             // static export
   images: {
-    unoptimized: true, // Required for GitHub Pages
+    unoptimized: true,          // Necessary for static sites (especially for GitHub Pages)
   },
-  basePath: "/bcarrier", // Adjust to your GitHub repository name
+  basePath: "/bcarrier",
   assetPrefix: "/bcarrier",
 };
 
-export default nextConfig;
+module.exports = nextConfig;
