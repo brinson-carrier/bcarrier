aae2f4ef1f50289d620c58e8290a4e3493bc111e
brinson-carrier <brinsoncarrier@gmail.com>
2024-12-17 14:24:22 -0500

    att 4 fix LI img

--- src/components/LIButton.tsx
+++ src/components/LIButton.tsx
@@ -12,7 +12,7 @@ const LIButton: React.FC<LIButtonProps> = ({ url }) => {
     <button>
         <a href={url} target="_blank" rel="noopener noreferrer" >
           <Image 
-            src={`${process.env.NEXT_PUBLIC_BASE_PATH || ''}/images/LI-In-Bug.png`} 
+            src={"/images/LI-In-Bug.png"} 
             alt="LinkedIn" 
             width={24} 
             height={24} 
